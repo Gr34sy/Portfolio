f4aaf021ecc64f40101d41b5419ae2abe3c94221
Gr34sy <slupek3@poczta.fm>
2022-12-21 19:33:11 +0100

    added typgraphy and all skillboxes

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -4,8 +4,30 @@ import { SkillBox } from "./SkillBox";
 
 export function SectionSkills(){
     return(
-        <section className="d-flex flex-wrap justify-content-center align-items-center section-skills">
-            <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" alt="Bootstrap" />
+        <section className="section-skills">
+            <div className="d-flex flex-wrap justify-content-center px-5 text-center skillboxes">
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" skill="Bootstrap" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" skill="React" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" skill="CSS" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" skill="HTML" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" skill="Visual Studio Code" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" skill="SASS" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg" skill="Material-UI"/>
+
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" skill="mySQL" />
+
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" skill="Firebase" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" skill="PHP" />
+
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" skill="Git"/>
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" skill="GitHub" />
+
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/apple/apple-original.svg" skill="macOS"/>
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/windows8/windows8-original.svg" skill="Windows" />
+
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" skill="Figma" />
+                <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xd/xd-plain.svg" skill="Adobe XD" />
+            </div>
         </section>
     )
 }
--- src/components/SkillBox.js
+++ src/components/SkillBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 
-export function SkillBox({src, alt}){
+export function SkillBox({src, skill}){
     return(
-        <div className="d-flex justify-content-between align-items-center skillbox">
-            <img alt={alt} width="50px" src={src} />
-            <h3 className="skill__description">asdads</h3>
+        <div className="d-flex justify-content-start align-items-center skillbox">
+            <img alt={skill} width="50px" src={src} />
+            <h3 className="skill__description">{skill}</h3>
         </div>
     )
 }
