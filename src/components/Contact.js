4f8be2805dba1a75804d94ed433cf54d5cab4267
Greasy <greasy@Greasys-MacBook-Pro.local>
2022-12-26 22:20:54 +0100

    added contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import separatorBottom from '../assets/separatorBottom.png';
 
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faPaperclip} from '@fortawesome/free-solid-svg-icons';
+import {faCode} from '@fortawesome/free-solid-svg-icons';
+
 export function Contact(){
     return(
         <>
@@ -8,8 +15,32 @@ export function Contact(){
             
         </figure>
 
-        <footer className='contact' id="contact">
-            
+        <footer className='p-5 contact' id="contact">
+            <h2 className='my-3 px-5'>Contact:</h2>
+            <a className="d-block text-decoration-none py-2 px-5 text-hover-gradient" href="mailto:slupek3@poczta.fm">
+                <span><FontAwesomeIcon icon={faEnvelope} className="text-hover-gradient" /></span>
+                Email
+            </a>
+
+            <a className="d-block text-decoration-none py-2 px-5 text-hover-gradient" href="https://www.linkedin.com/in/piotr-słupski-75821625b/">
+                <span><FontAwesomeIcon icon={faLinkedin} /></span>
+                LinkedIn
+            </a>
+
+            <a className="d-block text-decoration-none py-2 px-5 text-hover-gradient" href="https://github.com/Gr34sy">
+                <span><FontAwesomeIcon icon={faGithub} /></span>
+                Github
+            </a>
+
+            <a className="d-block text-decoration-none py-2 px-5 text-hover-gradient" href="https://www.codewars.com/users/Gr34sy">
+                <span><FontAwesomeIcon icon={faCode} /></span>
+                Codewars
+            </a>
+
+            <a className="d-block text-decoration-none py-2 px-5 text-hover-gradient" href="">
+                <span><FontAwesomeIcon icon={faPaperclip} /></span>
+                CV
+            </a>
         </footer>
         </>
     )
