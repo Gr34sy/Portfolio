5628347a1ffe58508a61c29bb46fd3c7d4f7764e
Greasy <slupek3@poczta.fm>
2022-12-27 16:33:48 +0100

    added hero box

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 export function Hero(){
     return(
-        <main className='hero' id="home">
-            
+        <main className='w-100 p-3 p-sm-5 d-flex align-items-center justify-content-center hero' id="home">
+
+            <div className="w-75 rounded border-none p-2 p-sm-5 m-0 mt-5 hero__box">
+                <h1 className='w-50 m-auto text-center'>About Me</h1>
+                <figure className='w-50 m-auto'>
+                    <img className="w-100" src="" alt="picture of developer"/>
+                </figure>
+            </div>
+
         </main>
     )
 }
