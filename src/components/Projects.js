ac3144822ce03e1ebe7f86a142c7cf5ee1a6fb1f
Gr34sy <slupek3@poczta.fm>
2023-03-09 18:12:42 +0100

    added bestshop project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,24 @@ import { ProjectCard } from './ProjectCard';
 import musicMania from '../assets/MusicMania.png';
 import oddam from '../assets/oddamwdobrerece.png';
 import spotifyClone from '../assets/SpotifyClone.png';
+import bestShop from '../assets/bestShop.png';
 
 export function Projects(){
     return(
         <section className='d-flex flex-wrap align-items-start justify-content-center p-1 p-sm-5 projects' id="projects">
             <h2 className='w-100 pt-5 text-center'>My projects</h2>
             <ProjectCard 
+            img = {bestShop}
+            description="My first RWD project. I also used there vanilla JavaScript and SASS. Site contains simple calculator." 
+            project='Best Shop Landing Page'
+            href={"https://inspiring-sorbet-4f9b9d.netlify.app"}
+            repo={"https://github.com/Gr34sy/BestShop-project"}
+            stack={['HTML', 'sass', 'javascript']}
+            />
+
+            <ProjectCard 
             img = {musicMania}
-            description="Page which connects with last.fm API. It enables filtering music artists, adding them to favorites and searching for the similar.Site takes a bit to load but give it time please.
-" 
+            description="Page which connects with last.fm API. It enables filtering music artists, adding them to favorites and searching for the similar.Site takes a bit to load but give it time please."
             project='Music Mania'
             href={"https://mellifluous-faun-63ea4b.netlify.app"}
             repo={"https://github.com/Gr34sy/MusicManiaProject"}
@@ -36,12 +45,13 @@ export function Projects(){
             stack={['HTML', 'Bootstrap', 'React']}
             />
 
+
             {/* <ProjectCard 
-            description="Some quick example text to build on the card title and make up the
-            bulk of the card's content." 
-            project={null}
-            href={"https://www.w3schools.com/bootstrap4/bootstrap_containers.asp"}
-            repo={"https://www.w3schools.com/bootstrap4/bootstrap_containers.asp"}
+            img = {}
+            description="" 
+            project={''}
+            href={""}
+            repo={""}
             stack={[]}
             /> */}
 
