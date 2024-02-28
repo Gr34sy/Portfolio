b5c229cd2205626fd6ee8c20260b1b682431c31e
Gr34sy <slupek3@poczta.fm>
2024-02-28 13:29:17 +0100

    updated icons in skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -23,42 +23,51 @@ export function SectionSkills(){
                     <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-plain.svg" skill="TypeScript" />
                 </div> */}
 
-                <h3 className="text-left mt-5 skill__type">
-                    IDE
-                </h3>
-                <div className="d-flex flex-wrap justify-content-start text-center w-100">
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" skill="Visual Studio Code" />
-                </div>
 
                 <h3 className="text-left mt-5 skill__type">
                     Front-end
                 </h3>
                 <div className="d-flex flex-wrap justify-content-start text-center w-100">
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" skill="CSS" />
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" skill="HTML" />
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" skill="SASS" />
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" skill="Bootstrap" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" skill="NextJS"/>
                     <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" skill="React" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" skill="SASS" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" skill="HTML" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" skill="CSS" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" skill="JavaScript" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" skill="TailwindCSS"/>
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" skill="Bootstrap" />
                     <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-plain.svg" skill="Material-UI"/>
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" skill="Next"/>
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" skill="TailwindCSS"/>
                 </div>
 
                 <h3 className="text-left mt-5 skill__type">
                     Back-end
                 </h3>
                 <div className="d-flex flex-wrap justify-content-start text-center w-100">
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" skill="Firebase" />
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg" skill="PHP" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" skill="NodeJS" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" skill="Express" />
+                </div>
+
+                <h3 className="text-left mt-5 skill__type">
+                    Databases
+                </h3>
+                <div className="d-flex flex-wrap justify-content-start text-center w-100">
                     <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" skill="MongoDB" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg" skill="Firebase" />
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg" skill="MySQL" />
                 </div>
 
                 <h3 className="text-left mt-5 skill__type">
                     Other Languages
                 </h3>
                 <div className="d-flex flex-wrap justify-content-start text-center w-100">
-                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" skill="mySQL" />
                     <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" skill="Python" />
+                </div>
+
+                <h3 className="text-left mt-5 skill__type">
+                    IDE
+                </h3>
+                <div className="d-flex flex-wrap justify-content-start text-center w-100">
+                    <SkillBox src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" skill="Visual Studio Code" />
                 </div>
 
                 <h3 className="text-left mt-5 skill__type">
